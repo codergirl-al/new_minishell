52ead2b711bbabb23acb10039a5b2d79cf6a9fe7
Albina Peposhi <codegirl.al@gmail.com>
2024-03-19 17:06:51 +0100

    Added cd builtin functionality

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/17 18:23:31 by apeposhi          #+#    #+#             */
-/*   Updated: 2024/03/19 11:23:10 by apeposhi         ###   ########.fr       */
+/*   Updated: 2024/03/19 16:22:44 by apeposhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,54 +19,45 @@ static int update_cwd(void)
 
     cwd = getcwd(NULL, 0);
     if (!cwd)
-	{
-		perror("minishell: error retrieving current directory");
-		return (1);
-	}
+		return (handle_error(1, "minishell: error retrieving current directory"));
 	result = update_env("PWD", cwd, 0);
 	free(cwd);
 	return (result == 0 ? 0 : 1);
 }
 
-static int	cd_to_home(void)
+static int	goto_home(char **env)
 {
 	char	*home;
 	
-	home = get_env_value("HOME");
+	home = ft_arrcmp((void **)env, "HOME");
 	if (!home)
 	{
 		ft_putstr_fd("minishell: cd: HOME not set\n", STDERR_FILENO);
 		return (1);
 	}
 	if (chdir(home) != 0)
-	{
-		perror("minishell: cd: error changing to HOME directory");
-		return (1);
-	}
+		return (handle_error(1, "minishell: cd: error changing to HOME directory"));
 	return (update_cwd());
 }
 
-static int cd_error_message(const char *path) {
+static int cd_error_message(char *path) {
 	ft_putstr_fd("minishell: cd: `", STDERR_FILENO);
 	ft_putstr_fd(path, STDERR_FILENO);
 	ft_putstr_fd("': No such file or directory\n", STDERR_FILENO);
 	return (1);
 }
 
-int b_cd(char *path)
+int b_cd(char *path, char **env)
 {
 	if (!path)
-		return (cd_to_home());
-
+		return (goto_home(env));
 	char *oldpwd = getcwd(NULL, 0);
 	if (chdir(path) != 0)
 	{
 		free(oldpwd);
 		return (cd_error_message(path));
 	}
-
 	update_env("OLDPWD", oldpwd, 0);
 	free(oldpwd);
-
 	return (update_cwd());
 }
