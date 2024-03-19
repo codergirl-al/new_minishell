e356ba5da39fd22570ec289dba1dfb3a7ed6f21d
Albina Peposhi <codegirl.al@gmail.com>
2024-03-19 17:07:43 +0100

    Added util functions

--- src/utils.c
+++ src/utils.c
@@ -6,16 +6,27 @@
 /*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 11:00:03 by apeposhi          #+#    #+#             */
-/*   Updated: 2024/03/19 12:39:09 by apeposhi         ###   ########.fr       */
+/*   Updated: 2024/03/19 16:21:49 by apeposhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "../include/minishell.h"
 
 int	update_env(char *name, char *old, int tof)
 {
 	return (0);
 }
 
-char	*get_env_value(char *val)
+int	handle_error(int r_value, char *r_message)
 {
-	return ("deeznuts");
+	perror(r_message);
+	return (r_value);
 }
+
+// char	*get_env_value(char **env, char *val)
+// {
+// 	char	*wanted_value;
+	
+// 	wanted_value = ft_arrcmp((void **)env, val);
+// 	return (wanted_value);
+// }
