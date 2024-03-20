718a56add6cfb3d2b301984993fe024f79b52e9d
Younes Kerdel <ykerdel@1-C-6.42heilbronn.de>
2024-03-20 15:06:33 +0100

    FIX: ft_strjoin work with flags

--- include/lib.h
+++ include/lib.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   lib.h                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ykerdel <ykerdel@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/15 11:10:15 by apeposhi          #+#    #+#             */
-/*   Updated: 2024/03/17 14:05:04 by apeposhi         ###   ########.fr       */
+/*   Updated: 2024/03/18 17:46:22 by ykerdel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,14 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define STRFREE_S1 (1 << 0)
+#define STRFREE_S2 (1 << 1)
+
 int		ft_issep(char c);
 int		ft_arrlen(char **arr);
 char	*ft_arrcmp(void **arr, void *keyword);
 char	**ft_arrdup(char **arr);
-char	*ft_strjoin_01(char *s1, char *s2);
-char	*ft_strjoin_11(char *s1, char *s2);
+char	*ft_strjoin(char *s1, char *s2, int free_flag);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 size_t	ft_strlen(const char *s);
 
