d174864dfd93c621329a2833527cd44abc0ec1de
Albina Peposhi <codegirl.al@gmail.com>
2024-03-24 15:40:27 +0100

    Added unset

--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -6,10 +6,38 @@
 /*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/17 18:23:45 by apeposhi          #+#    #+#             */
-/*   Updated: 2024/03/22 12:29:51 by apeposhi         ###   ########.fr       */
+/*   Updated: 2024/03/24 15:19:40 by apeposhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "../../include/minishell.h"
+#include "../../include/minishell.h"
 
-// void	b_unset(t_data *data, char **args) {}
+void	b_unset(char ***env, const char *key)
+{
+	size_t	i;
+	size_t	j;
+	size_t	key_len;
+	
+    if (!env || !*env || !key) {
+        fprintf(stderr, "Invalid arguments provided.\n");
+        return ;
+    }
+
+	key_len = strlen(key);
+	i = 0;
+	while ((*env)[i] != NULL)
+    {
+        if (strncmp((*env)[i], key, key_len) == 0 && (*env)[i][key_len] == '=')
+		{
+            free((*env)[i]);
+			j = i;
+			while ((*env)[j] != NULL)
+            {
+                (*env)[j] = (*env)[j + 1];
+				j++;
+            }
+            break;
+        }
+		i++;
+    }
+}
