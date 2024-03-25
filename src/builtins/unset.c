3f90cf457a79644ed15df3cbcfd1428821cad4a4
Albina Peposhi <codegirl.al@gmail.com>
2024-03-25 18:20:39 +0100

    Added changes

--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -6,35 +6,35 @@
 /*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/17 18:23:45 by apeposhi          #+#    #+#             */
-/*   Updated: 2024/03/25 18:18:17 by apeposhi         ###   ########.fr       */
+/*   Updated: 2024/03/25 18:19:46 by apeposhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-void	b_unset(t_data data, char **cmds)
+void	b_unset(char **env, char **cmds)
 {
 	size_t	i;
 	size_t	j;
 	size_t	k;
 	int	key_len;
 	
-	if (!data.envp || !cmds)
+	if (!env || !cmds)
 		return (handle_void_error("Invalid arguments provided.\n"));
 	k = 0;
 	while (cmds[++k])
 	{
 		i = 0;
 		key_len = ft_strlen(cmds[k]);
-		while ((data.envp)[i] != NULL)
+		while (env[i] != NULL)
 		{
-			if (strncmp((data.envp)[i], cmds[k], key_len) == 0 && (data.envp)[i][key_len] == '=')
+			if (strncmp(env[i], cmds[k], key_len) == 0 && env[i][key_len] == '=')
 			{
-				free((data.envp)[i]);
+				free(env[i]);
 				j = i;
-				while ((data.envp)[j] != NULL)
+				while (env[j] != NULL)
 				{
-					(data.envp)[j] = (data.envp)[j + 1];
+					env[j] = env[j + 1];
 					j++;
 				}
 				break;
